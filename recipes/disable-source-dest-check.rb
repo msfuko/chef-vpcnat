5ebeb9ebe1d753fe48848dd8a8b64518444984d9
msfuko <chloeleeq@gmail.com>
2014-12-30 14:37:40 +0800

    Update disable-source-dest-check.rb

--- recipes/disable-source-dest-check.rb
+++ recipes/disable-source-dest-check.rb
@@ -22,12 +22,6 @@ include_recipe 'awscli'
 instance_region = EC2Util.get_instance_region
 instance_id = EC2Util.get_instance_id
 
-execute 'disable Source/dest. check' do
-  command <<-EOH
-    aws ec2 
-    --region '#{instance_region}' 
-    modify-instance-attribute 
-    --instance-id '#{instance_id}'
-    --no-source-dest-check
-  EOH
+execute 'disable Source/dest.check' do
+  command "aws ec2 --region '#{instance_region}' modify-instance-attribute --instance-id '#{instance_id}' --no-source-dest-check"
 end
